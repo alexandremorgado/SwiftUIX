879b3def245748afa0bd302a15fb79f87d450c25
Alexandre Morgado <alexandremorgado@gmail.com>
2019-09-04 17:04:56 -0300

    Added Carthage support

---
+++ SwiftUIX/SwiftUIX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUIX.h
+//  SwiftUIX
+//
+//  Created by Alexandre Morgado on 04/09/19.
+//  Copyright © 2019 Alexandre Morgado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIX.
+FOUNDATION_EXPORT double SwiftUIXVersionNumber;
+
+//! Project version string for SwiftUIX.
+FOUNDATION_EXPORT const unsigned char SwiftUIXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIX/PublicHeader.h>
+
+
